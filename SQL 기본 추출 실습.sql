96f8cdd78082c146e56125105ceb25c8ec68c127
Yoonyoon <62419588+LeeJeongYoon95@users.noreply.github.com>
2022-04-17 17:07:33 +0900

    Delete SQL 기본 추출 실습.sql

--- SQL 기본 추출 실습.sql
+++
@@ -1,8 +0,0 @@
- SELECT fullVisitorId, 
-        COUNT(visitid) AS Visit_Count,
-        COUNTIF(device.deviceCategory = 'mobile') AS mobile,
-        COUNTIF(device.deviceCategory = 'desktop') AS desktop,
-        COUNTIF(device.deviceCategory = 'tablet') AS other
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` WHERE _table_suffix BETWEEN '20210701' AND '20210731'
-GROUP BY fullVisitorId
-ORDER BY Visit_Count ASC
