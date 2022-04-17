b9cd2b632dcf4aeaac6fc4c4ca98e6e9df8826bd
jeongyoon <hn02386@gmail.com>
2022-04-17 16:38:34 +0900

    SQL 기본 추출 실습

--- SQL 기본 추출 실습.sql
+++ SQL 기본 추출 실습.sql
@@ -5,4 +5,4 @@
         COUNTIF(device.deviceCategory = 'tablet') AS other
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` WHERE _table_suffix BETWEEN '20210701' AND '20210731'
 GROUP BY fullVisitorId
-ORDER BY Visit_Count DESC
+ORDER BY Visit_Count ASC
